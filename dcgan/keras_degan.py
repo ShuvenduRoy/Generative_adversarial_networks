08218f7e1e62c6ae8c9767fac9297343a124f199
Bikash <bikash11roy@gmail.com>
2018-03-22 01:22:01 +0600

    defining generator and discriminator

--- dcgan/keras_degan.py
+++ dcgan/keras_degan.py
@@ -14,9 +14,97 @@ import numpy as np
 
 class DCGAN():
     """Implementation of Deep convolutional GAN"""
+
     def __init__(self):
         self.img_rows = 28
         self.img_cols = 28
         self.channels = 1
 
         optimizer = Adam(0.0002, 0.5)
+
+        # Build and compile the discriminator
+        self.discriminator = self.build_discriminator()
+        self.discriminator.compile(loss='binary_crossentropy',
+                                   optimizer=optimizer,
+                                   metrics=['accuracy'])
+
+        # Build and compile the generator
+        self.generator = self.build_generator()
+        self.generator.compile(loss='binary_crossentropy', optimizer=optimizer)
+
+        # generator input
+        z = Input(shape=(100,))
+        img = self.generator(z)
+
+        # for the combined model, we don't train the discriminator
+        self.discriminator.trainable = False
+
+        # valid takes generated image and decide validity
+        valid = self.discriminator(img)
+
+        # combined model (stacked generator and discriminator) takes
+        # noise as input => generated images => determines validity
+        self.combined = Model(z, valid)
+        self.combined.compile(loss='binary_crossentropy', optimizer=optimizer)
+
+    def build_generator(self):
+        noise_shape = (100,)
+
+        model = Sequential()
+        model.add(Dense(128 * 7 * 7, activation="relu", input_shape=noise_shape))
+        model.add(Reshape((7, 7, 128)))
+        model.add(BatchNormalization(momentum=0.8))
+
+        model.add(UpSampling2D())
+        model.add(Conv2D(128, kernel_size=3, padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(momentum=0.8))
+
+        model.add(UpSampling2D())
+        model.add(Conv2D(64, kernel_size=3, padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(momentum=0.8))
+
+        model.add(Conv2D(1, kernel_size=3, padding="same"))
+        model.add(Activation("tanh"))
+
+        model.summary()
+
+        noise = Input(shape=noise_shape)  # input to model
+        img = model(noise)                # output of the model
+
+        return Model(noise, img)
+
+    def build_discriminator(self):
+        img_shape = (self.img_rows, self.img_cols, self.channels)
+
+        model = Sequential()
+
+        model.add(Conv2D(32, kernel_size=3, strides=2, input_shape=img_shape, padding="same"))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dropout(0.25))
+
+        model.add(Conv2D(64, kernel_size=3, strides=2, padding="same"))
+        model.add(ZeroPadding2D(padding=((0, 1), (0, 1))))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dropout(0.25))
+        model.add(BatchNormalization(momentum=0.8))
+
+        model.add(Conv2D(128, kernel_size=3, strides=2, padding="same"))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dropout(0.25))
+        model.add(BatchNormalization(momentum=0.8))
+
+        model.add(Conv2D(256, kernel_size=3, strides=1, padding="same"))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dropout(0.25))
+
+        model.add(Flatten())
+        model.add(Dense(1, activation='sigmoid'))
+
+        model.summary()
+
+        img = Input(shape=img_shape)
+        validity = model(img)
+
+        return Model(img, validity)
