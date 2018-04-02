495c8ea250fe06368675051c89793cfb78e96312
Bikash <bikash11roy@gmail.com>
2018-04-02 20:44:17 +0600

    UTK face data loader added

--- utils.py
+++ utils.py
@@ -7,6 +7,41 @@ import matplotlib.pyplot as plt
 from torchvision import datasets, transforms
 
 
+def UTKFace_data():
+    import glob
+    import numpy as np
+    import matplotlib.image as mpimage
+    import matplotlib.pyplot as plt
+    import cv2
+
+    def load_data(size=(28, 28)):
+        print("load data started")
+        all_images = glob.glob("E:\\Datasets\\UTKFace\\*")
+
+        X = []
+        y = []
+
+        for i, image in enumerate(all_images):
+            # # take half data
+            # if i%20 != 0:
+            #     continue
+
+            only_name = image.split('\\')[-1]
+            age = int(only_name.split('_')[0])
+
+            img = mpimage.imread(image)
+            img = cv2.resize(img, (size[0], size[1]))
+
+            X.append(img)
+            y.append(int(age / 5))
+
+        X = np.array(X).astype(np.float32)
+        y = np.array(y).astype(np.int)
+        print("data loaded")
+
+        return X, y
+
+
 def load_mnist(dataset):
     data_dir = os.path.join("./data", dataset)
 
