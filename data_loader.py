6f553b11a683850f3d7d62379f773c4446d3d194
Bikash <bikash11roy@gmail.com>
2018-04-02 20:47:23 +0600

    dataloader added

---
+++ data_loader.py
@@ -0,0 +1,33 @@
+import glob
+import numpy as np
+import matplotlib.image as mpimage
+import matplotlib.pyplot as plt
+import cv2
+
+
+def UTKFace_data(size=(128, 218)):
+    print("load data started")
+    all_images = glob.glob("E:\\Datasets\\UTKFace\\*")
+
+    X = []
+    y = []
+
+    for i, image in enumerate(all_images):
+        # # take half data
+        # if i%20 != 0:
+        #     continue
+
+        only_name = image.split('\\')[-1]
+        age = int(only_name.split('_')[0])
+
+        img = mpimage.imread(image)
+        img = cv2.resize(img, (size[0], size[1]))
+
+        X.append(img)
+        y.append(int(age / 5))
+
+    X = np.array(X).astype(np.float32)
+    y = np.array(y).astype(np.int)
+    print("data loaded")
+
+    return X, y
