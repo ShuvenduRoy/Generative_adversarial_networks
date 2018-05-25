4976393af65c4e44dbb03086dc95bf12bd3bbbb5
Bikash <bikash11roy@gmail.com>
2018-05-25 23:28:22 +0600

    dataset name updated

--- bicycle/main.py
+++ bicycle/main.py
@@ -54,7 +54,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--epoch', type=int, default=0, help='epoch to start training from')
     parser.add_argument('--n_epochs', type=int, default=200, help='number of epochs of training')
-    parser.add_argument('--dataset_name', type=str, default="edge2shoes", help='name of the dataset')
+    parser.add_argument('--dataset_name', type=str, default="edges2shoes", help='name of the dataset')
     parser.add_argument('--batch_size', type=int, default=8, help='size of the batches')
     parser.add_argument('--lr', type=float, default=0.0002, help='adam: learning rate')
     parser.add_argument('--b1', type=float, default=0.5, help='adam: decay of first order momentum of gradient')
